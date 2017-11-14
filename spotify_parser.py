ebd260bbbc76f1f1017ce8bba23708048bebec6b
polomex <p.talarico@hotmail.com>
2017-11-13 21:29:22 -0500

    Update spotify_parser.py

--- spotify_parser.py
+++ spotify_parser.py
@@ -1,16 +1,10 @@
 #!/usr/python3
 
-
 from spotipy.oauth2 import SpotifyClientCredentials
 import os, sys
 import spotipy
 import spotipy.util as util
 import xlsxwriter
-
-
-if len(sys.argv) < 2:
-	print("You must enter the user name of your account")
-	exit
 
 dev_username = sys.argv[1]
 CBOLD = '\033[97m'
@@ -22,8 +16,7 @@ CEND = '\033[0m'
 class Song:
 	def __init__(self):
 		self.data = []
-		# sp = ''
-
+		
 	def request_saved_tracks():
 		username = dev_username
 		scope = 'user-library-read'
@@ -38,6 +31,7 @@ class Song:
 				worksheet = workbook.add_worksheet()
 				row = 0
 				col = 0
+				print("[*] Building xlsx file")
 
 				for i in range(number_of_songs):
 					off += 40
@@ -69,10 +63,6 @@ class Song:
 						temprow.append(song_features['analysis_url'])
 						temprow.append(song_features['duration_ms'])
 						temprow.append(song_features['time_signature'])
-	
-						# for song_att in song_features:
-						# 	temparray.append(song_att)
-						
 						array.append(temprow)
 					for lists in array:
 						if lists[0] != '':
@@ -81,27 +71,16 @@ class Song:
 								worksheet.write(row, col+counter1, song_info)
 								counter1 += 1
 							row += 1
-						else:
-							break
-	
 				workbook.close()
 			make_xlsx(50)
 			print(CGREEN + "[*] DONE!" + CEND)
-	
 		else:
 			print(CRED + "Can't get token for " + sys.argv[1] + CEND)
 
-
-
 def main():
-	print(CBOLD + """
-     Spotify Song Parser
-      <--06/11/2017-->
-      """ + CEND)
 	if len(sys.argv) < 1:
 		print("You must provide a username as argument")
 	else:
 		Song.request_saved_tracks()
 
-##################################
 main()
